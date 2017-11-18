90c452db69efa5b303e4447e9033d0975d62f700
Romulo-FanHero <romulo@fanhero.com>
2017-11-18 13:57:23 -0200

    feature-salesforce-added

--- shame.js
+++ shame.js
@@ -90,7 +90,7 @@ request.post(`${BASE_URL}/login`, { json: { email: EMAIL, password: PASSWORD } }
     .then(evts => promise.map(
 
         // filter unmapped types
-        evts.filter(e => e.state !== 'UNMAPPED' && e.state !== 'MAPPED' && !inPattern(e.name, EVENT_EXCLUSION_PATTERN) && (e.name.includes('production') || e.name.includes('dataflux'))),
+        evts.filter(e => e.state !== 'UNMAPPED' && e.state !== 'MAPPED' && !inPattern(e.name, EVENT_EXCLUSION_PATTERN) && (e.name.includes('production') || e.name.includes('dataflux') || e.name.includes('salesforce'))),
 
         evt => {
 
